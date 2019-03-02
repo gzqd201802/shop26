7dd2ef26b778a763dba701a9624f2b96541e650d
DaSu <gzqd201802@163.com>
2019-03-02 17:24:18 +0800

    生命周期函数，商品列表页获取页面启动参数

--- src/pages/goods_list/index.vue
+++ src/pages/goods_list/index.vue
@@ -1,11 +1,32 @@
 <template>
   <view>
-    商品列表页
+    商品列表页 {{ keyword }}
   </view>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      keyword:''
+    }
+  },
+  onLoad(options){
+    console.log("A页面加载onLoad",options);
+    this.keyword = options.keyword;
+  },
+  onShow(){
+    console.log("B页面加载onShow");
+  },
+  onReady(){
+    console.log("C页面加载onReady");
+  },
+  onHide(){
+    console.log("页面隐藏onHide");
+  },
+  onUnload(){
+    console.log("页面卸载onUnload");
+  }
 
 }
 </script>
